d3f499e8069201229f222548feaf28a11db0c045
John DeNero <papajohn@gmail.com>
2016-09-30 11:37:40 -0700

    render alignments

---
+++ src/render.py
@@ -0,0 +1,17 @@
+"""Render alignments"""
+
+import sys
+from align import *
+
+if __name__ == "__main__":
+    """Usage: render.py data.zh data.en data.gold data.guess"""
+    data_paths = sys.argv[1:5]
+    data_files = [open(f) for f in data_paths]
+
+    for i, (zh, en, gold, guess) in enumerate(zip(*data_files)):
+        zh_words = zh.lower().split()
+        en_words = en.lower().split()
+        gold_align = Aligned(zh_words, en_words, gold.strip())
+        guess_align = Aligned(zh_words, en_words, guess.strip())
+        print("Alignment %d:" % i)
+        print(gold_align.render(guess_align.sure))
